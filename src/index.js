b999a8fde08d76d02b2e3be296031d102f103075
NaniNoni <stoneater03@gmail.com>
2022-07-12 17:34:09 +0200

    Adds the ability to check values of all of the squares

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 let word = "ptaki";
 let wordAsArray = word.split("");
-let line_1 = document.getElementsByClassName("line-1");
-function GetValues(line) {
-    let values = new Array();
-    for (let i = 0; i < line.length; i++) {
-        values.push(line.values);
-    }
-    return;
+const line_1 = document.getElementsByClassName("line-1");
+const line_2 = document.getElementsByClassName("line-2");
+const line_3 = document.getElementsByClassName("line-3");
+const line_4 = document.getElementsByClassName("line-4");
+const line_5 = document.getElementsByClassName("line-5");
+for (let i = 0; i < CheckAllSquares().length; i++) {
+    console.log(CheckAllSquares()[i]);
 }
-console.log(line_1.values);
-console.log(typeof (line_1.values));
+function CheckAllSquares() {
+    // Setting all of the squares
+    let squares = new Array();
+    squares[0] = line_1[0].value;
+    squares[1] = line_1[1].value;
+    squares[2] = line_1[2].value;
+    squares[3] = line_1[3].value;
+    squares[4] = line_1[4].value;
+    squares[5] = line_2[0].value;
+    squares[6] = line_2[1].value;
+    squares[7] = line_2[2].value;
+    squares[8] = line_2[3].value;
+    squares[9] = line_2[4].value;
+    squares[10] = line_3[0].value;
+    squares[11] = line_3[1].value;
+    squares[12] = line_3[2].value;
+    squares[13] = line_3[3].value;
+    squares[14] = line_3[4].value;
+    squares[15] = line_4[0].value;
+    squares[16] = line_4[1].value;
+    squares[17] = line_4[2].value;
+    squares[18] = line_4[3].value;
+    squares[19] = line_4[4].value;
+    squares[20] = line_5[0].value;
+    squares[21] = line_5[1].value;
+    squares[22] = line_5[2].value;
+    squares[23] = line_5[3].value;
+    squares[24] = line_5[4].value;
+    return squares;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,58 @@
 let word = "ptaki"
 let wordAsArray = word.split("");
 
-let line_1: any = document.getElementsByClassName("line-1");
+const line_1 = document.getElementsByClassName("line-1");
+const line_2 = document.getElementsByClassName("line-2");
+const line_3 = document.getElementsByClassName("line-3");
+const line_4 = document.getElementsByClassName("line-4");
+const line_5 = document.getElementsByClassName("line-5");
 
-function GetValues (line) {
-
-    let values: string[] = new Array();
-
-    for (let i = 0; i < line.length; i ++) {
-        values.push(line.values);
-    }
-
-    return 
+for (let i = 0; i < CheckAllSquares().length; i ++) {
+    console.log(CheckAllSquares()[i]);
 }
 
-console.log(line_1.values);
-console.log(typeof(line_1.values));
+
+
+
+
+
+
+
+
+
+
+function CheckAllSquares (): string[] {
+    // Setting all of the squares
+    let squares: string[] = new Array();
+    squares[0] = (<HTMLInputElement>line_1[0]).value;
+    squares[1] = (<HTMLInputElement>line_1[1]).value;
+    squares[2] = (<HTMLInputElement>line_1[2]).value;
+    squares[3] = (<HTMLInputElement>line_1[3]).value;
+    squares[4] = (<HTMLInputElement>line_1[4]).value;
+
+    squares[5] = (<HTMLInputElement>line_2[0]).value;
+    squares[6] = (<HTMLInputElement>line_2[1]).value;
+    squares[7] = (<HTMLInputElement>line_2[2]).value;
+    squares[8] = (<HTMLInputElement>line_2[3]).value;
+    squares[9] = (<HTMLInputElement>line_2[4]).value;
+
+    squares[10] = (<HTMLInputElement>line_3[0]).value;
+    squares[11] = (<HTMLInputElement>line_3[1]).value;
+    squares[12] = (<HTMLInputElement>line_3[2]).value;
+    squares[13] = (<HTMLInputElement>line_3[3]).value;
+    squares[14] = (<HTMLInputElement>line_3[4]).value;
+
+    squares[15] = (<HTMLInputElement>line_4[0]).value;
+    squares[16] = (<HTMLInputElement>line_4[1]).value;
+    squares[17] = (<HTMLInputElement>line_4[2]).value;
+    squares[18] = (<HTMLInputElement>line_4[3]).value;
+    squares[19] = (<HTMLInputElement>line_4[4]).value;
+
+    squares[20] = (<HTMLInputElement>line_5[0]).value;
+    squares[21] = (<HTMLInputElement>line_5[1]).value;
+    squares[22] = (<HTMLInputElement>line_5[2]).value;
+    squares[23] = (<HTMLInputElement>line_5[3]).value;
+    squares[24] = (<HTMLInputElement>line_5[4]).value;
+
+    return squares;
+}
